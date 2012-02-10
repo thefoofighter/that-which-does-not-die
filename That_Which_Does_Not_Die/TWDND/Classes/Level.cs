09f63930f45fd39a97524c4cdf5a4ec193de45aa
Cathal McNally <mcnallycathal@yahoo.ie>
2012-02-10 03:21:42 +0000

    Signed-off-by: Cathal McNally <mcnallycathal@yahoo.ie>

--- That_Which_Does_Not_Die/TWDND/Classes/Level.cs
+++ That_Which_Does_Not_Die/TWDND/Classes/Level.cs
@@ -8,7 +8,6 @@ namespace Twdnd
     static class Level
     {
         private static Room[,] rooms;
-
         #region properties
         public static Room[,] Rooms
         {
@@ -20,42 +19,55 @@ namespace Twdnd
         {
             BuildLevel();
         }
+     
+
 
         //BuildLevel()
         private static void BuildLevel()
         {
-            rooms = new Room[2, 2];
+
+            rooms = new Room[10, 10];
             Room room;
             Item item;
 
-            #region Room[0,0]
-                
-            room = new Room();
+            #region Starting Room (Hospital bed - Room 1) [0,0]
 
+            room = new Room();
             // Assign this room to location 0,0
             rooms[0, 0] = room;
-
-            //set room properties
-            room.Title = "Red Room";
-            room.Description = "You have entered the red room, There is a locked door to the South";
+            room.Title = "Hospital Bedroom";
+            room.Description = "You awake in a hospital Bedroom, bleary eyed and confused, You find it hard to focus on anything in particular, you stumble out of the bed and fall to your knees.'Where am I?', 'How did i get here?'.";
             room.AddExits(Direction.East);
 
             // create a new item
             item = new Item();
-            
-            //setup the item
-            item.Title = "Blue Ball";
-            item.PickupText = "You just picked up the Blue Ball";
+            item.Title = "PDA";
+            item.PickupText = "You picked up a PDA, the SD card slot is empty.";
 
-            // add item to room
             room.Items.Add(item);
             #endregion
+
+            #region Room 2 [1,0]
+            room = new Room();
+            // Assign this room to location 0,0
+            rooms[1,0] = room;
+            room.Title = "Room 2";
+            room.Description = "You now find yourself in room 2 after struggling to gain the strength to walk there. The light is flickering and irritates your eyes.";
+            room.AddExits(Direction.East);
+            room.AddExits(Direction.South);
+
+            // create a new item
+            item = new Item();
+            item.Title = "Apple";
+            item.PickupText = "You picked up an Apple, Its not exactly fresh but still edible.";
+
+            room.Items.Add(item);
+            #endregion
+
 
             //place the player in the starting room
             Player.PosX = 0;
             Player.PosY = 0;
-
-
-        }
+        } 
     }
 }
